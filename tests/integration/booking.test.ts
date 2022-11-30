1e3f47f0964c972f01fddcb310e334c6b834d4ff
nicollekoetzler <nickkoetzler@gmail.com>
2022-11-30 17:34:35 -0300

    created route get /booking

---
+++ tests/integration/booking.test.ts
@@ -0,0 +1,32 @@
+// import app, { init } from "@/app";
+// import { prisma } from "@/config";
+// import faker from "@faker-js/faker";
+// import { TicketStatus } from "@prisma/client";
+// import e from "express";
+// import httpStatus from "http-status";
+// import * as jwt from "jsonwebtoken";
+// import supertest from "supertest";
+// import {
+//   createEnrollmentWithAddress,
+//   createUser,
+//   createTicketType,
+//   createTicket,
+//   createPayment,
+//   generateCreditCardData,
+//   createTicketTypeWithHotel,
+//   createTicketTypeRemote,
+//   createHotel,
+//   createRoomWithHotelId,
+// } from "../factories";
+// import { cleanDb, generateValidToken } from "../helpers";
+
+// beforeAll(async () => {
+//   await init();
+// });
+
+// beforeEach(async () => {
+//   await cleanDb();
+// });
+
+// const server = supertest(app);
+
